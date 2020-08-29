866061e9b7e6fba08ee7bad4b34a4815d108bdb4
deepakm14035 <34639761+deepakm14035@users.noreply.github.com>
2020-08-29 10:28:31 +0530

    levels+glow

--- arcadegame1/Assets/prefabs/ShakeAnim.cs
+++ arcadegame1/Assets/prefabs/ShakeAnim.cs
@@ -7,16 +7,16 @@ public class ShakeAnim : MonoBehaviour
     public Vector3[] m_waypoints;
 
     int m_currentWaypoint = 0;
-    public float m_speed = 0.001f;
+    public float m_speed = 0.0002f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         m_waypoints = new Vector3[3];
-        m_waypoints[0] = new Vector3(transform.localScale.x + 0.05f, transform.localScale.y, 1f);
+        m_waypoints[0] = new Vector3(transform.localScale.x + 0.08f, transform.localScale.y, 1f);
         m_waypoints[1] = transform.localScale;
-        m_waypoints[2] = new Vector3(transform.localScale.x, transform.localScale.y + 0.05f, 1f);
+        m_waypoints[2] = new Vector3(transform.localScale.x, transform.localScale.y + 0.08f, 1f);
 
 
     }
@@ -25,13 +25,12 @@ public class ShakeAnim : MonoBehaviour
     void Update()
     {
 
-        if (Vector3.Distance(transform.localScale, m_waypoints[m_currentWaypoint]) < 0.001f)
+        if (Vector3.Distance(transform.localScale, m_waypoints[m_currentWaypoint]) < 0.01f)
         {
             m_currentWaypoint++;
             m_currentWaypoint = m_currentWaypoint % m_waypoints.Length;
         }
-        else
-            transform.localScale += Vector3.Normalize(m_waypoints[m_currentWaypoint] - transform.localScale) * m_speed * Time.deltaTime;
+        transform.localScale += Vector3.Normalize(m_waypoints[m_currentWaypoint] - transform.localScale) * m_speed * Time.deltaTime*0.05f;
 
     }
 }
